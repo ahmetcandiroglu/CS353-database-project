29026cf16eefa9650085358ab01853440ae1945b
Ahmet Çandıroğlu <ahmetcandiroglu@gmail.com>
2018-05-14 19:29:59 +0300

    Fix add venue page

--- src/add_venue.php
+++ src/add_venue.php
@@ -6,13 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/bootstrap-3.1.1.min.css" type="text/css" />
-    <link rel="stylesheet" href="css/bootstrap-multiselect.css" type="text/css" />
-
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
-    <script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap-multiselect.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
     <title>Add Venue!</title>
   </head>
@@ -21,14 +17,10 @@
 
   		<?php include_once "navigation.php"; ?>
 
-        <div class="row col-lg-12">&nbsp;</div>
-        <div class="row col-lg-12">&nbsp;</div>
-        <div class="row col-lg-12">&nbsp;</div>
-        <div class="row col-lg-12">&nbsp;</div>
-        <div class="row col-lg-12">&nbsp;</div>
+        <br> <br>
+        
         <div class="row">
-            <div class="col-lg-3"></div>
-            <div class="col-lg-6">
+            <div class="col-lg-6 offset-lg-3">
  
             <form action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> " method="post">
 
@@ -64,7 +56,7 @@
                                             <span class="help-block"><?php //echo $lastName_err; ?></span>
                                         </div>
                                     </div>  
-                                     <div class="col-lg-3">
+                                    <div class="col-lg-3">
                                         <div class="form-group <?php //echo (!empty($lastName_err)) ? 'has-error' : ''; ?>">
                                             <b><label>City </label></b>
                                             <div class="dropdown">
@@ -208,9 +200,9 @@
                     </form>
                 </div>
             </div>
-        <
-
- <?php include_once "footer.php" ?>
+        </body>
+    </head>
+</html>
 
 
 
